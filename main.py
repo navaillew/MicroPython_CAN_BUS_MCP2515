3cf224ab9b03d2a24529e260f104039b6f480c10
Wesley N <wnavaille@gmail.com>
2026-02-03 23:39:00 -0500

    gitignore

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
-'''
-A simple exmaple to send data to can bus
-'''
-
+from machine import Pin
 import sys
 import time
-
 from canbus import Can, CanError, CanMsg, CanMsgFlag
 
-# setup
-can = Can()
+can = Can(spi=0,spics=8)
 
-# initilize
 ret = can.begin()
 if ret != CanError.ERROR_OK:
     print("Error to initilize can!")
     sys.exit(1)
 print("initlized succesufully!")
-
-# send
-while True:
-    data = b"\x12\x34\x56\x78\x9A\xBC\xDE\xF0"
-    
-    # standard format frame message
-    msg = CanMsg(can_id=0x123, data=data)
-    error = can.send(msg)
-    if error == CanError.ERROR_OK:
-        print('1------------------------------')
-            
-    # extended format frame message
-    msg = CanMsg(can_id=0x12345678, data=data, flags=CanMsgFlag.EFF)
-    error = can.send(msg)
-    if error == CanError.ERROR_OK:
-        print('2------------------------------')
-
-    time.sleep(1)
